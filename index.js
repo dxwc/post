75a7951ecf115d0e6e9138d898df3947fd0077c1
dxwc <dxwc@users.noreply.github.com>
2018-01-08 23:21:36 -0500

    working full feed with minimum <entry> elements

--- index.js
+++ index.js
@@ -1,9 +1,100 @@
-const fs       = require('fs');
-const yaml     = require('js-yaml');
-const sqlite3  = require('sqlite3');
-const readline = require('readline');
-const uuidv4   = require('uuid/v4');
-const exec     = require('child_process').exec;
+const fs        = require('fs');
+const yaml      = require('js-yaml');
+const sqlite3   = require('sqlite3');
+const readline  = require('readline');
+const uuidv4    = require('uuid/v4');
+const exec      = require('child_process').exec;
+const validator = require('validator');
+
+let feed;
+let feed_id;
+let md_dir = './markdown';
+let all_entries = '';
+
+open_db_global()
+.then(() =>
+{
+    return db_get_promise('SELECT id FROM just_id');
+})
+.then((data) =>
+{
+    if(data && data.id) feed_id = data.id;
+    return db_get_promise('SELECT count(*) as `count` FROM entry');
+})
+.then((data) =>
+{
+    feed = yaml.safeLoad(fs.readFileSync('./_feed.yaml', { encoding : 'utf-8'}));
+    if(feed.id === undefined || feed.title === undefined)
+        throw new Error('feed id or title not found in ./__feed.yaml file');
+
+    if(data.count === 0 && feed_id === undefined)
+    {
+        feed_id = undefined; // won't be needing any longer
+        return db_get_promise(`INSERT INTO just_id(id) VALUES('${feed.id}')`);
+    }
+    else if(feed_id !== feed.id && data.count > 0)
+    {
+        console.log('PERMANENT FEED ID CHANGED AND THERE ARE EXISTING ENTRRIES!');
+        console.log('THIS SHOULDN\'T HAPPEN!');
+        console.log('THE CORRECT ID IS (set it in _feed.yaml if changed accidently):');
+        console.log(feed_id);
+        throw new Error();
+    }
+    else if(feed_id !== feed.id && data.count === 0)
+    {
+        return read_line_promise('Do you want to change feed ID? [y/n]')
+        .then((answer) =>
+        {
+            if(answer === 'y' || answer === 'Y')
+            {
+                console.log('--Updating');
+                return db_run_promise(`UPDATE just_id set id = '${feed.id}'`);
+            }
+            else
+            {
+                console.log('--Keeping existing ID in database');
+                console.log('--You must update the ./_feed.yml id field to:');
+                console.log(feed_id);
+                feed.id = feed_id;
+            }
+        });
+    }
+    else
+    {
+        return; // all good
+    }
+})
+.then(() =>
+{
+    return traverse_md_files(md_dir);
+})
+.then(() =>
+{
+    fs.writeFileSync
+    (
+        './atom.xml',
+        generate_feed(feed, all_entries),
+        { encoding : 'utf-8'}
+    );
+    console.log('--Generated ./atom.xml file');
+    db.close();
+})
+.catch((err) =>
+{
+    console.log('Error in chain:\n', err);
+    db.close();
+});
+
+
+
+
+
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+
+
+
+
 
 // >------------- Sqlite3 callbacks promisify ------------->
 
@@ -37,11 +128,18 @@ function open_db_global()
                 (
                     id       TEXT PRIMARY KEY, -- UUID v4
                     updated  TEXT NOT NULL, -- update if content is different
-                    file_loc TEXT NOT NULL, -- location of md file
+                    file_loc TEXT NOT NULL UNIQUE, -- location of md file
                     content  TEXT -- html/text content
                 );
                 `
             )
+            .then(() =>
+            {
+                return sql_promise
+                (
+                    `CREATE INDEX IF NOT EXISTS file_loc_i ON entry(file_loc);`
+                );
+            })
             .then(() =>
             {
                 return sql_promise
@@ -173,6 +271,8 @@ let xml = `<?xml version='1.0' encoding='utf-8'?>
     ${category_generator(feed.categories)}
     ${person_construct_generator(feed.contributors, 'contributor')}
 
+    ${entries}
+
 </feed>`;
 
     xml = xml.replace(/^\s*[\r\n]/gm, ''); // https://stackoverflow.com/a/16369725
@@ -180,7 +280,6 @@ let xml = `<?xml version='1.0' encoding='utf-8'?>
 }
 
 // <------------- Generator and helpers <-------------
-
 
 /* Extracts yaml within '---\n' and '\n---'. Rest are assumed to be markdown */
 function extract_from_md(file)
@@ -192,80 +291,155 @@ function extract_from_md(file)
 
     if(m1 === -1 || m2 === -1) throw new Error('Expected yaml seperators not found');
 
-    obj.yaml = all.substr(m1+4, m2-5-m1);
+    obj.yaml = yaml.safeLoad(all.substr(m1+4, m2-5-m1));
     obj.md = all.substr(m2+5);
 
     return obj;
 }
 
-
-// -----------------------------------------------------------------------------------
-
-// Feed init generation example:
-
-let feed_id;
-let feed;
-
-open_db_global()
-.then(() =>
+function traverse_md_files(markdown_root_dir)
 {
-    return db_get_promise('SELECT id FROM just_id');
-})
-.then((data) =>
-{
-    if(data && data.id) feed_id = data.id;
-    return db_get_promise('SELECT count(*) as `count` FROM entry');
-})
-.then((data) =>
-{
-    feed = yaml.safeLoad(fs.readFileSync('./_feed.yaml', { encoding : 'utf-8'}));
-    if(feed.id === undefined || feed.title === undefined)
-        throw new Error('feed id or title not found in ./__feed.yaml file');
+    let inside_dir = fs.readdirSync(markdown_root_dir, { encoding : 'utf8'});
 
-    if(data.count === 0 && feed_id === undefined)
+    let promise_chain = Promise.resolve();
+    let obj;
+
+    for(let i = 0; i < inside_dir.length; ++i)
     {
-        feed_id = undefined; // won't be needing any longer
-        return db_get_promise(`INSERT INTO just_id(id) VALUES('${feed.id}')`);
-    }
-    else if(feed_id !== feed.id && data.count > 0)
-    {
-        console.log('PERMANENT FEED ID CHANGED AND THERE ARE EXISTING ENTRRIES!');
-        console.log('THIS SHOULDN\'T HAPPEN!');
-        console.log('THE CORRECT ID IS (set it in _feed.yaml if changed accidently):');
-        console.log(feed_id);
-        throw new Error();
-    }
-    else if(feed_id !== feed.id && data.count === 0)
-    {
-        return read_line_promise('Do you want to change feed ID? [y/n]')
-        .then((answer) =>
+        if(fs.statSync(markdown_root_dir + '/' + inside_dir[i]).isDirectory())
         {
-            if(answer === 'y' || answer === 'Y')
+            traverse_md_files(markdown_root_dir + '/' + inside_dir[i]);
+        }
+        else if(inside_dir[i].slice(-2) == 'md')
+        {
+            // TODO: see if it is a good idea to make it async instead of chain
+            let loc = markdown_root_dir + '/' + inside_dir[i];
+            promise_chain = promise_chain.then(() =>
             {
-                console.log('--Updating');
-                return db_run_promise(`UPDATE just_id set id = '${feed.id}'`);
+                return check_entry(loc, extract_from_md(loc));
+            });
+        }
+        else if(inside_dir[i][0] !== '.')
+        {
+            // non .md or temporary/hidden file, action: copy
+        }
+    }
+
+    return promise_chain;
+}
+
+function run_command(command)
+{
+    return new Promise((resolve, reject) =>
+    {
+        console.log('Executing:', command);
+        exec(command, (err, stdout, stderr) =>
+        {
+            if(err)
+            {
+                reject(err);
             }
             else
             {
-                console.log('--Keeping existing ID in database');
-                console.log('--You must update the ./_feed.yml id field to:');
-                console.log(feed_id);
-                feed.id = feed_id;
+                // if(stdout) console.info('Stdout:', stdout);
+                if(stderr) console.error('Stderr', stderr);
+                resolve([stdout, stderr]);
             }
         });
-    }
-    else
+    });
+}
+
+/* md_file === full [relative to root md_dir] file location of a md file */
+function check_entry(md_file_loc, data)
+{
+    let extracted_md = data.md;
+    if(md_file_loc === undefined || extracted_md === undefined)
+        throw new Error('md_file or extracted_md was undefined');
+
+    md_file_loc = validator.escape(md_file_loc);
+    extracted_md = validator.escape(extracted_md);
+
+    let content_diff = false; // indicate need to generate
+    let entry_id = uuidv4();
+    let updated = new Date().toISOString();
+
+    // TODO, date update/insert -- check yaml/file-change for info, else current
+    // TODO, replace content with hash to save space
+
+    return db_get_promise(`SELECT * FROM entry where file_loc='${md_file_loc}'`)
+    .then((result) =>
     {
-        return; // all good
-    }
-})
-.then(() =>
-{
-    console.log(generate_feed(feed));
-    db.close();
-})
-.catch((err) =>
-{
-    console.log('Error in chain:\n', err);
-    db.close();
-});
+        if(result === undefined)
+        {
+            content_diff = true;
+            return db_run_promise
+            (
+                `
+                INSERT INTO entry
+                (
+                    id,
+                    updated,
+                    file_loc,
+                    content
+                )
+                VALUES
+                (
+                    '${entry_id}',
+                    '${updated}',
+                    '${md_file_loc}',
+                    '${extracted_md}'
+                )
+                `
+            );
+        }
+        else
+        {
+            entry_id = result.id;
+            content_diff = (result.content !== extracted_md);
+
+            if(content_diff)
+            {
+                return db_run_promise
+                (
+                    `
+                    UPDATE entry
+                    SET
+                        content = '${extracted_md}',
+                        updated = '${updated}'
+                    WHERE
+                        id = '${entry_id}'
+                    `
+                );
+            }
+            else
+            {
+                updated = result.updated;
+            }
+        }
+    })
+    .then(() =>
+    {
+        return run_command
+        (
+            `pandoc -f markdown -t html5 ${validator.unescape(md_file_loc)}`
+        );
+    })
+    .then((result) =>
+    {
+        all_entries +=
+`
+  <entry>
+    <id>${entry_id}</id>
+    <title>${data.yaml.title}</title>
+    <updated>${updated}</updated>
+    <content type='html'>
+    ${validator.escape(result[0])}
+    </content>
+  </entry>
+`;
+    })
+    .then(() =>
+    {
+        return { content_diff : content_diff, entry_id : entry_id };
+    });
+}
---
+++ markdown/a_folder/example.md
@@ -0,0 +1,17 @@
+---
+title: Example Page
+category:
+    - example
+    - page
+tag:
+    - yaml
+    - markdown
+    - test
+    - debug
+---
+
+# Example Page
+
+Hello World!
+
+[example link](https://example.com)
